37427ab34261512048cacd33bb73fe71daccd081
leslie <leslie.wilton18@tectijuana.edu.mx>
2025-09-11 15:26:50 -0700

    f

--- Api/db.js
+++ Api/db.js
@@ -1,15 +1,16 @@
-
 import mysql from "mysql";  // Importa el paquete mysql para conectar con la base de datos
 import dotenv from "dotenv";
 
 dotenv.config();
+
 // Crea y exporta la conexión a la base de datos MySQL con los datos de configuración
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST,// Dirección del servidor de base de datos
-    user: process.env.DB_USER, // Usuario para conectar a la base de datos
-    password: process.env.DB_PASSWORD, // Contraseña del usuario
-    database: process.env.DB_NAME,  // Nombre de la base de datos a usar
+  host: process.env.DB_HOST,           // Dirección del servidor de base de datos
+  port: Number(process.env.DB_PORT),   // Puerto proporcionado por Aiven (asegúrate que esté en .env)
+  user: process.env.DB_USER,           // Usuario para conectar a la base de datos
+  password: process.env.DB_PASSWORD,   // Contraseña del usuario
+  database: process.env.DB_NAME,       // Nombre de la base de datos a usar
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: true           // Para validar el certificado SSL
   }
 });
